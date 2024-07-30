dc2b9ff4f3d86411c72b4a738d26a6c1f2e25eeb
Vov4ukz53 <vov4ukz53@gmail.com>
2024-07-30 15:06:03 +0200

    Add colors and change container width

--- src/theme.js
+++ src/theme.js
@@ -1,18 +1,19 @@
 const theme = {
    colors: {
+      brightBlue: "hsl(220, 98%, 61%)",
       veryLightGray: "hsl(0, 0%, 98%)",
       veryLightGrayishBlue: "hsl(236, 33%, 92%)",
       lightGrayishBlue: "hsl(233, 11%, 84%)",
       darkGrayishBlue: "hsl(236, 9%, 61%)",
-      veryDarkGrayishBlue: "hsl(235, 19%, 35%)"
+      veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+      hoverButton: "linear-gradient(hsl(192, 100%, 67%),hsl(280, 87%, 65%))",
    },
    breakpoints: {
       tablets: 992,
       mobile: 767,
    },
    containers: {
-      mobile: 375,
-      desktop: 1440
+      main: 570
    }
 };
 
